639a0c4c7614fd56554e700f85b40adfd234d7fa
GuiRomanholi <guilhermeroma343@gmail.com>
2024-08-06 09:21:18 -0300

    fazendo botoes e mexendo nas listas com js

--- js/script.js
+++ js/script.js
@@ -33,3 +33,57 @@ for (let indice in frutas){
 }
 
 console.log('--------------------')
+
+//Adicionar um item no array(push -> add no final)
+frutas.push("abacaxi");
+
+//No inicio com UNSHIFT() -> add no começo
+frutas.unshift("melancia")
+
+console.log('--------------------')
+
+//Função para remover um item do final do Array
+document.getElementById("btnRemoveFinal").addEventListener("click", ()=>{
+    frutas.pop();
+    addFrutas();
+});
+
+console.log('--------------------')
+
+//Função para remover um item do início do Array
+document.getElementById("btnRemoveInicio").addEventListener("click", ()=>{
+    frutas.shift();
+    addFrutas();
+});
+
+//Colocando em ordem alfabética o array
+document.getElementById("OrdemA").addEventListener("click", ()=>{
+    frutas.sort();
+    addFrutas();
+});
+
+//Revertendo a ordem apresentada
+document.getElementById("Revert").addEventListener("click", ()=>{
+    frutas.reverse();
+    addFrutas();
+});
+
+
+
+
+
+
+//Criar função para adicionar o arra a lista de frutas no HTML
+function addFrutas(){
+    let listaFrutas = document.getElementById("lista-frutas");
+    listaFrutas.innerHTML = "";
+    for (let fruta of frutas){
+        let item = document.createElement("li");
+        item.textContent = fruta;
+        listaFrutas.appendChild(item);
+    }
+}
+
+//Chamando a função addFrutas() através do botão AddFrutas
+document.getElementById("btnAddFrutas").addEventListener("click", addFrutas);
+//Basicamente fizemos a lista aparecer na tela, apertando o botão, colocando toda a lista ali.
