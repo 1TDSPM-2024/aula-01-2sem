b4e1243409498af61764e126545241e6fb051f6f
Erick0105 <erickspot01@gmail.com>
2024-08-06 12:10:22 -0300

    exercício concluido

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //? Declaração de Arrays
-let aFrutas = ["maça", "banana", "laranja", "morango","jambo"];
+let aFrutas = ["Maça", "Banana", "Laranja", "Morango","Jambo"];
 
 //* Lendo o Array
 console.log(aFrutas);
@@ -37,7 +37,8 @@ aFrutas.push("Abacaxi");
 //No ínicio com unshift()
 aFrutas.unshift("Melancia")
 
-function addFrutas() {
+//* Função que mostrar as frutas
+function mostrarFrutas() {
     let elListaFrutas = document.getElementById("listaFrutas");
     elListaFrutas.innerHTML = ""
 
@@ -47,9 +48,56 @@ function addFrutas() {
         elListaFrutas.appendChild(item);
     }
 }
+document.getElementById("btnMostrarFrutas").addEventListener("click", mostrarFrutas);
 
-document.getElementById("btnAddFrutas").addEventListener("click", addFrutas);
+//* Parte que remove a ultima fruta da lista
+document.getElementById("removerFrutaFinal").addEventListener("click", ()=>{
+    aFrutas.pop();
+    mostrarFrutas();
+})
 
+//* Parte que remove a primeira fruta da lista
+document.getElementById("removerFrutaInicio").addEventListener("click", ()=>{
+    aFrutas.shift(); //! essa função elimina o primeiro item de uma array
+    mostrarFrutas();
+})
 
+//* Parte que organiza a lista em ordem alfabetica
+document.getElementById("ordernar").addEventListener("click", ()=>{
+    aFrutas.sort(); //! Só a função sem parametros organiza a array em ordem alfabética
+    mostrarFrutas();
+})
 
+//* Parte que reverte a ordem apresentada
+document.getElementById("reverter").addEventListener("click", ()=>{
+    aFrutas.reverse(); //! Essa função reverte a ordem da array
+    mostrarFrutas();
+})
 
+function pesquisaFruta(fruta) {
+    let valorPesquisado = aFrutas.indexOf(fruta);
+    if (valorPesquisado >= 0) {
+        document.getElementById("itemFruta").innerHTML = aFrutas[valorPesquisado];
+        return valorPesquisado;
+    } else {
+
+        document.getElementById("itemFruta").innerHTML = "Fruta não encontrada!";
+        return -1;
+    }
+}
+
+document.getElementById("btnPesquisa").addEventListener("click", ()=>{
+    pesquisaFruta(document.getElementById("idFruta").value);
+})
+
+document.getElementById("btnRemover").addEventListener("click", () => {
+
+    let indiceDaFruta = pesquisaFruta(document.getElementById("idFruta").value);
+
+    if (indiceDaFruta >= 0) {
+        aFrutas.splice(indiceDaFruta, 1); //! Esse comando faz com que nesse caso remova o apenas 1 item a partir do indice da fruta dado, isso contando ela
+    } else {
+        document.getElementById("itemFruta").innerHTML = "ITEM INEXISTENTE!";
+    }
+    mostrarFrutas();
+});
